e13e1c0fad31436165f3d98f45c4956fd5567463
Roman Indermühle <127976131+romanindermuehle@users.noreply.github.com>
2026-02-01 20:29:20 +0100

    Add logging

--- Sources/VaporDemo/routes.swift
+++ Sources/VaporDemo/routes.swift
@@ -9,6 +9,21 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("error") { req async -> String in
+        app.logger.error("This is an error")
+        return "Error logged"
+    }
+    
+    app.get("warning") { req async -> String in
+        app.logger.warning("This is a warning")
+        return "Warning logged"
+    }
+    
+    app.get("info") { req async -> String in
+        app.logger.info("This is some info")
+        return "Info logged"
+    }
 
     try app.register(collection: TodoController())
 }
